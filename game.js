ce158f0ccac51417ea40b5b59a7df6deec119451
Inder <inderyyz@gmail.com>
2018-07-04 00:29:58 +1000

    game win logic updated

--- game.js
+++ game.js
@@ -15,25 +15,83 @@ var playerMsg = document.querySelector("body h1")
 
 var gameboard=document.querySelector(".gameboard")
 
+var winFlag = false
+var playerColor 
+
 var playerTurn = function(event) {
     if (turnCounter % 2 == 0) {
-        if (event.target.style.backgroundColor == "white") {return}
+        if (event.target.style.backgroundImage != "") {return}
         playerMsg.innerText="PLAYER 1 - Your Turn" 
         event.target.style.backgroundColor="pink"
         event.target.style.backgroundImage="url(StickerO.webp)"
+        checkWin("pink")
         turnCounter = turnCounter + 1
     }
     else {
-        if (event.target.style.backgroundColor == "white") {return}
+        if (event.target.style.backgroundImage != "") {return}
         playerMsg.innerText="PLAYER 2 - Your Turn"
         event.target.style.backgroundColor="blue"
         event.target.style.backgroundImage="url(StickerX.webp)"
+        checkWin("blue")
         turnCounter = turnCounter + 1
     }
 }
 
-//var checkWin = function() {
- //   if (b1Btn.style.backgroundColor=="blue" && b2Btn.style.backgroundColor=="blue" && b3Btn.style.backgroundColor=="blue")
+var checkWin = function (playerColor) {
+    
+    checkRow(playerColor)
+    checkCol(playerColor)
+    checkDiag(playerColor)
+}
+
+        var checkRow = function (playerColor) {
+            if (b1Btn.style.backgroundColor===playerColor && b1Btn.style.backgroundColor===b2Btn.style.backgroundColor && b2Btn.style.backgroundColor===b3Btn.style.backgroundColor) 
+                {console.log(playerColor + "Top Row Wins")
+                return winFlag=true}
+            else if (b4Btn.style.backgroundColor===playerColor && b4Btn.style.backgroundColor===b5Btn.style.backgroundColor && b5Btn.style.backgroundColor===b6Btn.style.backgroundColor) 
+                {console.log(playerColor + "Middle Row Wins")
+                 return winFlag=true}
+            else if (b7Btn.style.backgroundColor===playerColor && b7Btn.style.backgroundColor===b8Btn.style.backgroundColor && b8Btn.style.backgroundColor===b9Btn.style.backgroundColor) 
+                 {console.log(playerColor + "Bottom Row Wins")
+                  return winFlag=true}
+            else {winFlag=false}
+
+        }
+
+
+        var checkCol = function (playerColor) {
+            if (b1Btn.style.backgroundColor===playerColor && b1Btn.style.backgroundColor===b4Btn.style.backgroundColor && b4Btn.style.backgroundColor===b7Btn.style.backgroundColor) 
+                {console.log(playerColor + "Left Column Wins")
+                return winFlag=true}
+            else if (b2Btn.style.backgroundColor===playerColor && b2Btn.style.backgroundColor===b5Btn.style.backgroundColor && b5Btn.style.backgroundColor===b8Btn.style.backgroundColor) 
+                {console.log(playerColor + "Middle Column Wins")
+                 return winFlag=true}
+            else if (b3Btn.style.backgroundColor===playerColor && b3Btn.style.backgroundColor===b6Btn.style.backgroundColor && b6Btn.style.backgroundColor===b9Btn.style.backgroundColor) 
+                 {console.log(playerColor + "Right Column Wins")
+                  return winFlag=true}
+            else {winFlag=false}
+
+        }
+
+
+
+        var checkDiag = function (playerColor) {
+            if (b1Btn.style.backgroundColor===playerColor && b1Btn.style.backgroundColor===b5Btn.style.backgroundColor && b5Btn.style.backgroundColor===b9Btn.style.backgroundColor) 
+                {console.log(playerColor + "Right Diagonal Wins")
+                return winFlag=true}
+            else if (b3Btn.style.backgroundColor===playerColor && b3Btn.style.backgroundColor===b5Btn.style.backgroundColor && b5Btn.style.backgroundColor===b7Btn.style.backgroundColor) 
+                {console.log(playerColor + "Left Diagonal Wins")
+                 return winFlag=true}
+            else {winFlag=false}
+
+        }
+
+        
+
+
+
+
+
 
 
 gameboard.addEventListener('click',playerTurn)
